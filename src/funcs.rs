0b395e4928a1aadb3d762fa32d53ebc1b3be8876
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-12-13 23:14:18 -0300

    Functions back in a single file again.

--- src/funcs.rs
+++ src/funcs.rs
@@ -6,9 +6,10 @@ use std::ffi::c_void;
 use crate::co;
 use crate::com::{PPVtbl, Vtbl};
 use crate::ffi::{comctl32, kernel32, ole32, user32};
-use crate::handles::HWND;
+use crate::handles::{HINSTANCE, HWND};
 use crate::internal_defs::parse_multi_z_str;
-use crate::structs::{CLSID, GUID, MSG, RECT};
+use crate::structs::{ATOM, CLSID, GUID, MSG, RECT, WNDCLASSEX};
+use crate::Utf16;
 
 /// [`AdjustWindowRectEx`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-adjustwindowrectex)
 /// function.
@@ -182,5 +183,68 @@ pub fn IsGUIThread(bConvert: bool) -> Result<bool, co::ERROR> {
 		}
 	} else {
 		Ok(r != 0)
+	}
+}
+
+/// [`PeekMessage`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-peekmessagew)
+/// function
+pub fn PeekMessage(lpMsg: &mut MSG, hWnd: HWND,
+	wMsgFilterMin: u32, wMsgFilterMax: u32, wRemoveMsg: co::PM) -> bool
+{
+	unsafe {
+		user32::PeekMessageW(
+			lpMsg as *mut MSG as *mut c_void,
+			hWnd.as_ptr(),
+			wMsgFilterMin,
+			wMsgFilterMax,
+			wRemoveMsg.into(),
+		) != 0
+	}
+}
+
+/// [`PostQuitMessage`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-postquitmessage)
+/// function.
+pub fn PostQuitMessage(nExitCode: i32) {
+	unsafe { user32::PostQuitMessage(nExitCode) }
+}
+
+/// [`RegisterClassEx`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassexw)
+/// function.
+pub fn RegisterClassEx(lpwcx: &WNDCLASSEX) -> Result<ATOM, co::ERROR> {
+	match unsafe {
+		user32::RegisterClassExW(lpwcx as *const WNDCLASSEX as *const c_void)
+	} {
+		0 => Err(GetLastError()),
+		atom => Ok(ATOM::from(atom)),
+	}
+}
+
+/// [`SetLastError`](https://docs.microsoft.com/en-us/windows/win32/api/errhandlingapi/nf-errhandlingapi-setlasterror)
+/// function.
+pub fn SetLastError(dwErrCode: co::ERROR) {
+	unsafe { kernel32::SetLastError(dwErrCode.into()) }
+}
+
+/// [`TranslateMessage`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-translatemessage)
+/// function.
+pub fn TranslateMessage(lpMsg: &MSG) -> bool {
+	unsafe {
+		user32::TranslateMessage(lpMsg as *const MSG as *const c_void) != 0
+	}
+}
+
+/// [`UnregisterClass`](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-unregisterclassw)
+/// function.
+pub fn UnregisterClass(
+	lpClassName: &str, hInstance: HINSTANCE) -> Result<(), co::ERROR>
+{
+	match unsafe {
+		user32::UnregisterClassW(
+			Utf16::from_str(lpClassName).as_ptr(),
+			hInstance.as_ptr(),
+		)
+	} {
+		0 => Err(GetLastError()),
+		_ => Ok(()),
 	}
 }
--- src/funcs/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Win32 free functions.
-
-mod funcs_am;
-mod funcs_nz;
-
-pub use funcs_am::*;
-pub use funcs_nz::*;
