2eaf7342edfb9e7cc4a1283ed021db6d0bffae91
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-12-21 12:50:20 -0300

    Static variable for auto_ctrl_id().

--- src/gui/native_control_base.rs
+++ src/gui/native_control_base.rs
@@ -1,5 +1,7 @@
 use crate::handles::HWND;
 
+static mut BASE_CTRL_ID: u16 = 20_000; // in-between Visual Studio Resource Editor values
+
 /// Base to all native child controls.
 #[derive(Clone)]
 pub struct NativeControlBase {
@@ -9,7 +11,11 @@ pub struct NativeControlBase {
 
 impl NativeControlBase {
 	pub fn auto_ctrl_id() -> u16 {
-		0
+		unsafe {
+			let new_id = BASE_CTRL_ID;
+			BASE_CTRL_ID += 1;
+			new_id
+		}
 	}
 
 	pub fn new_with_id(ctrl_id: u16) -> NativeControlBase {
