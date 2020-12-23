944773f64cd58492e1a3fd6860fc972aacd2dd55
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2020-12-22 23:53:23 -0300

    WindowMainOpts.

--- src/gui/window_main.rs
+++ src/gui/window_main.rs
@@ -5,8 +5,9 @@ use crate::funcs as f;
 use crate::gui::events::Events;
 use crate::gui::Parent;
 use crate::gui::window_base::WindowBase;
-use crate::handles::{HINSTANCE, HWND};
+use crate::handles::{HACCEL, HBRUSH, HCURSOR, HICON, HINSTANCE, HMENU, HWND};
 use crate::internal_defs::str_dyn_error;
+use crate::structs::SIZE;
 
 /// Main application window.
 #[derive(Clone)]
@@ -16,7 +17,7 @@ pub struct WindowMain {
 
 impl WindowMain {
 	/// Creates a new `WindowMain` object.
-	pub fn new() -> WindowMain {
+	pub fn new(opts: WindowMainOpts) -> WindowMain {
 		Self {
 			base: WindowBase::new(),
 		}
@@ -44,6 +45,7 @@ impl WindowMain {
 		}
 
 		f::InitCommonControls();
+		// GLOBAL FONT...
 
 
 		let hinst = HINSTANCE::GetModuleHandle(None)
@@ -57,4 +59,78 @@ impl Parent for WindowMain {
 	fn on(&self) -> Events {
 		self.base.on()
 	}
+}
+
+//------------------------------------------------------------------------------
+
+/// Options for [`WindowMain::new`](crate::gui::WindowMain::new).
+#[derive(Default)]
+pub struct WindowMainOpts {
+	/// Window class name to be
+	/// [registered](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassexw).
+	///
+	/// Defaults to an auto-generated string.
+	pub class_name: String,
+	/// Window class styles to be
+	/// [registered](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassexw).
+	///
+	/// Defaults to `co::WS::DBLCLKS`.
+	pub class_style: co::WS,
+	/// Window main icon to be
+	/// [registered](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassexw).
+	///
+	/// Defaults to no icon.
+	pub class_icon: HICON,
+	/// Window cursor to be
+	/// [registered](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassexw).
+	///
+	/// Defaults to `co::IDC::ARROW`.
+	pub class_cursor: HCURSOR,
+	/// Window background brush to be
+	/// [registered](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-registerclassexw).
+	///
+	/// Defaults to `co::COLOR::BTNFACE`.
+	pub class_bg_brush: HBRUSH,
+
+	/// Window title to be
+	/// [created](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexw).
+	///
+	/// Defaults to empty string.
+	pub title: String,
+	/// Size of window client area, in pixels, to be
+	/// [created](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexw).
+	/// Does not include title bar or borders.
+	///
+	/// Will be adjusted to match current system DPI.
+	///
+	/// Defaults to 600x500.
+	pub size: SIZE,
+	/// Window styles to be
+	/// [created](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexw).
+	///
+	/// Defaults to `co::WS::CAPTION | co::WS::SYSMENU | co::WS::CLIPCHILDREN | co::WS::BORDER`.
+	///
+	/// Suggestions:
+	/// * `co::WS::SIZEBOX` to make the window resizable;
+	/// * `co::WS::MINIMIZEBOX` to have a minimize button;
+	/// * `co::WS::MAXIMIZEBOX` to have a maximize button.
+	pub style: co::WS,
+	/// Extended window styles to be
+	/// [created](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexw).
+	///
+	/// Defaults to `co::WS_EX::LEFT`.
+	pub ex_style: co::WS_EX,
+	/// Main menu of the window to be
+	/// [created](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexw).
+	///
+	/// This menu is not shared, the window will own it, and destroy it when the
+	/// window is destroyed.
+	///
+	/// Defaults to absent.
+	pub menu: HMENU,
+	/// Main accelerator table of the window to be
+	/// [created](https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-createwindowexw).
+	///
+	/// Defaults to absent.
+	pub accel_table: HACCEL,
 }
