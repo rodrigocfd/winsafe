539c490045e9b6dc8ed443e5c719314f9970125c
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-01-08 14:03:15 -0300

    Edit control notifications.

--- src/gui/events/edit_events.rs
+++ src/gui/events/edit_events.rs
@@ -21,6 +21,16 @@ impl EditEvents {
 		unsafe { &*self.parent_events }
 	}
 
+	cmd_event! { en_align_ltr_ec, co::CMD::EN_ALIGN_LTR_EC,
+		/// [`EN_ALIGN_LTR_EC`](https://docs.microsoft.com/en-us/windows/win32/controls/en-align-ltr-ec)
+		/// command notification.
+	}
+
+	cmd_event! { en_align_rtl_ec, co::CMD::EN_ALIGN_RTL_EC,
+		/// [`EN_ALIGN_LTR_EC`](https://docs.microsoft.com/en-us/windows/win32/controls/en-align-rtl-ec)
+		/// command notification.
+	}
+
 	cmd_event! { en_change, co::CMD::EN_CHANGE,
 		/// [`EN_CHANGE`](https://docs.microsoft.com/en-us/windows/win32/controls/en-change)
 		/// command notification.
@@ -42,6 +52,16 @@ impl EditEvents {
 		/// ```
 	}
 
+	cmd_event! { en_err_space, co::CMD::EN_ERRSPACE,
+		/// [`EN_ERRSPACE`](https://docs.microsoft.com/en-us/windows/win32/controls/en-errspace)
+		/// command notification.
+	}
+
+	cmd_event! { en_h_scoll, co::CMD::EN_HSCROLL,
+		/// [`EN_HSCROLL`](https://docs.microsoft.com/en-us/windows/win32/controls/en-hscroll)
+		/// command notification.
+	}
+
 	cmd_event! { en_kill_focus, co::CMD::EN_KILLFOCUS,
 		/// [`EN_KILLFOCUS`](https://docs.microsoft.com/en-us/windows/win32/controls/en-killfocus)
 		/// command notification.
@@ -49,6 +69,21 @@ impl EditEvents {
 
 	cmd_event! { en_max_text, co::CMD::EN_MAXTEXT,
 		/// [`EN_MAXTEXT`](https://docs.microsoft.com/en-us/windows/win32/controls/en-maxtext)
+		/// command notification.
+	}
+
+	cmd_event! { en_set_focus, co::CMD::EN_SETFOCUS,
+		/// [`EN_SETFOCUS`](https://docs.microsoft.com/en-us/windows/win32/controls/en-setfocus)
+		/// command notification.
+	}
+
+	cmd_event! { en_update, co::CMD::EN_UPDATE,
+		/// [`EN_UPDATE`](https://docs.microsoft.com/en-us/windows/win32/controls/en-update)
+		/// command notification.
+	}
+
+	cmd_event! { en_v_scoll, co::CMD::EN_VSCROLL,
+		/// [`EN_VSCROLL`](https://docs.microsoft.com/en-us/windows/win32/controls/en-vscroll)
 		/// command notification.
 	}
 }
