11808f6bf10826c5c6f9c8f27f9fe808ef108877
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-02-23 13:47:27 -0300

    List box events.

---
+++ src/gui/events/events_lbn.rs
@@ -0,0 +1,79 @@
+use std::ptr::NonNull;
+
+use crate::co;
+use crate::gui::events::MsgEvents;
+use crate::gui::traits::Parent;
+
+/// Exposes list box control
+/// [notifications](https://docs.microsoft.com/en-us/windows/win32/controls/bumper-list-box-control-reference-notifications).
+///
+/// These event methods are just proxies to the
+/// [`MsgEvents`](crate::gui::events::MsgEvents) of the parent window, who is
+/// the real responsible for the child event handling.
+///
+/// You cannot directly instantiate this object, it is created internally by the
+/// control.
+pub struct ListBoxEvents {
+	parent_user_events: NonNull<MsgEvents>, // used only before parent creation
+	ctrl_id: u16,
+}
+
+impl ListBoxEvents {
+	pub(crate) fn new(parent: &dyn Parent, ctrl_id: u16) -> ListBoxEvents {
+		Self {
+			parent_user_events: NonNull::from(parent.user_events_ref()), // convert reference to pointer
+			ctrl_id,
+		}
+	}
+
+	fn parent_user_events(&self) -> &MsgEvents {
+		unsafe { self.parent_user_events.as_ref() }
+	}
+
+	cmd_event! { lbn_dbl_clk, co::LBN::DBLCLK.into(),
+		/// [`LBN_DBLCLK`](https://docs.microsoft.com/en-us/windows/win32/controls/lbn-dblclk)
+		/// notification.
+		///
+		/// Notifies the application that the user has double-clicked an item in a
+		/// list box.
+	}
+
+	cmd_event! { lbn_err_space, co::LBN::ERRSPACE.into(),
+		/// [`LBN_ERRSPACE`](https://docs.microsoft.com/en-us/windows/win32/controls/lbn-errspace)
+		/// notification.
+		///
+		/// Notifies the application that the list box cannot allocate enough
+		/// memory to meet a specific request.
+	}
+
+	cmd_event! { lbn_kill_focus, co::LBN::KILLFOCUS.into(),
+		/// [`LBN_KILLFOCUS`](https://docs.microsoft.com/en-us/windows/win32/controls/lbn-killfocus)
+		/// notification.
+		///
+		/// Notifies the application that the list box has lost the keyboard focus.
+	}
+
+	cmd_event! { lbn_sel_cancel, co::LBN::SELCANCEL.into(),
+		/// [`LBN_SELCANCEL`](https://docs.microsoft.com/en-us/windows/win32/controls/lbn-selcancel)
+		/// notification.
+		///
+		/// Notifies the application that the user has canceled the selection in a
+		/// list box.
+	}
+
+	cmd_event! { lbn_sel_change, co::LBN::SELCHANGE.into(),
+		/// [`LBN_SELCHANGE`](https://docs.microsoft.com/en-us/windows/win32/controls/lbn-selchange)
+		/// notification.
+		///
+		/// Notifies the application that the selection in a list box has changed
+		/// as a result of user input.
+	}
+
+	cmd_event! { lbn_set_focus, co::LBN::SETFOCUS.into(),
+		/// [`LBN_SETFOCUS`](https://docs.microsoft.com/en-us/windows/win32/controls/lbn-setfocus)
+		/// notification.
+		///
+		/// Notifies the application that the list box has received the keyboard
+		/// focus.
+	}
+}
