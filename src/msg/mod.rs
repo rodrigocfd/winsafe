a0067603d28ff3dca83848e01ef85fc3d4bb2b8c
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-03-04 10:21:17 -0300

    Improving msg docs.

--- src/msg/mod.rs
+++ src/msg/mod.rs
@@ -1,8 +1,33 @@
 //! Parameters of
 //! [window messages](https://docs.microsoft.com/en-us/windows/win32/winmsg/about-messages-and-message-queues).
 //!
-//! Each message struct defines the parameters it receives and also its return
-//! type.
+//! [`WndMsg`](crate::msg::WndMsg) is the generic message, with `WPARAM` and
+//! `LPARAM` fields. Other messages belong to a module according to its prefix,
+//! for example, [`BM_CLICK`](crate::msg::bm::Click) can be found in
+//! [`bm`](crate::msg::bm) module.
+//!
+//! # Examples
+//!
+//! Sending [`LVM_DELETEITEM`](crate::msg::lvm::DeleteItem) message with
+//! [`SendMessage`](crate::HWND::SendMessage) to delete the 3rd element in a
+//! [`ListView`](crate::gui::ListView) control. Notice how the `index` parameter
+//! is passed when initializing the message struct, and how it returns a
+//! `WinResult`.
+//!
+//! ```rust,ignore
+//! use winsafe::{HWND, msg::lvm};
+//!
+//! let hlistview: HWND; // initialize it somewhere...
+//!
+//! hlistview.SendMessage({
+//!     lvm::DeleteItem {
+//!         index: 2,
+//!     },
+//! }).expect("Failed to delete item 2.");
+//! ```
+//!
+//! Each message has its own set of parameters. Also, each message has its own
+//! result type, which will be returned by `SendMessage`.
 
 #[macro_use]
 mod macros;
