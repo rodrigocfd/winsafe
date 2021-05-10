8f30adbc10dec6ef519c2393f42ba3bd146cb443
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-05-10 11:45:24 -0300

    MessageBox constants docs.

--- src/co/consts_mn.rs
+++ src/co/consts_mn.rs
@@ -3,38 +3,131 @@ use crate::co::CCM;
 const_type! { MB, u32,
 	/// [`MessageBox`](crate::HWND::MessageBox) `uType` (`u32`).
 	=>
+	/// The message box contains three push buttons: Abort, Retry, and Ignore.
 	ABORTRETRYIGNORE, 0x00000002
+	/// The message box contains three push buttons: Cancel, Try Again,
+	/// Continue. Use this message box type instead of
+	/// [`MB::ABORTRETRYIGNORE`](crate::co::MB::ABORTRETRYIGNORE).
 	CANCELTRYCONTINUE, 0x00000006
+	/// Adds a Help button to the message box. When the user clicks the Help
+	/// button or presses F1, the system sends a
+	/// [`WM_HELP`](crate::msg::wm::Help) message to the owner.
 	HELP, 0x00004000
+	/// The message box contains one push button: OK. This is the default.
 	OK, 0x00000000
+	/// The message box contains two push buttons: OK and Cancel.
 	OKCANCEL, 0x00000001
+	/// The message box contains two push buttons: Retry and Cancel.
 	RETRYCANCEL, 0x00000005
+	/// The message box contains two push buttons: Yes and No.
 	YESNO, 0x00000004
+	/// The message box contains three push buttons: Yes, No, and Cancel.
 	YESNOCANCEL, 0x00000003
 
+	/// An exclamation-point icon appears in the message box.
 	ICONEXCLAMATION, 0x00000030
+	/// An exclamation-point icon appears in the message box.
 	ICONWARNING, Self::ICONEXCLAMATION.0
+	/// An icon consisting of a lowercase letter i in a circle appears in the
+	/// message box.
 	ICONINFORMATION, 0x00000040
+	/// An icon consisting of a lowercase letter i in a circle appears in the
+	/// message box.
 	ICONASTERISK, Self::ICONINFORMATION.0
+	/// A question-mark icon appears in the message box. The question-mark
+	/// message icon is no longer recommended because it does not clearly
+	/// represent a specific type of message and because the phrasing of a
+	/// message as a question could apply to any message type. In addition,
+	/// users can confuse the message symbol question mark with Help
+	/// information. Therefore, do not use this question mark message symbol in
+	/// your message boxes. The system continues to support its inclusion only
+	/// for backward compatibility.
 	ICONQUESTION, 0x00000020
+	/// A stop-sign icon appears in the message box.
 	ICONSTOP, Self::ICONERROR.0
+	/// A stop-sign icon appears in the message box.
 	ICONERROR, 0x00000010
+	/// A stop-sign icon appears in the message box.
 	ICONHAND, Self::ICONERROR.0
 
+	/// The first button is the default button. `MB::DEFBUTTON1` is the default
+	/// unless [`MB::DEFBUTTON2`](crate::co::MB::DEFBUTTON2),
+	/// [`MB::DEFBUTTON3`](crate::co::MB::DEFBUTTON3), or
+	/// [`MB::DEFBUTTON4`](crate::co::MB::DEFBUTTON4) is specified.
 	DEFBUTTON1, 0x00000000
+	/// The second button is the default button.
 	DEFBUTTON2, 0x00000100
+	/// The third button is the default button.
 	DEFBUTTON3, 0x00000200
+	/// The fourth button is the default button.
 	DEFBUTTON4, 0x00000300
 
+	/// The user must respond to the message box before continuing work in the
+	/// window identified by the hWnd parameter. However, the user can move to
+	/// the windows of other threads and work in those windows.
+	///
+	/// Depending on the hierarchy of windows in the application, the user may
+	/// be able to move to other windows within the thread. All child windows of
+	/// the parent of the message box are automatically disabled, but pop-up
+	/// windows are not.
+	///
+	/// `MB::APPLMODAL` is the default if neither
+	/// [`MB::SYSTEMMODAL`](crate::co::MB::SYSTEMMODAL) nor
+	/// [`MB::TASKMODAL`](crate::co::MB::TASKMODAL) is specified.
 	APPLMODAL, 0x00000000
+	/// Same as [`MB::APPLMODAL`](crate::co::MB::APPLMODAL) except that the
+	/// message box has the [`WS_EX::TOPMOST`](crate::co::WS_EX::TOPMOST) style.
+	/// Use system-modal message boxes to notify the user of serious,
+	/// potentially damaging errors that require immediate attention (for
+	/// example, running out of memory). This flag has no effect on the user's
+	/// ability to interact with windows other than those associated with hWnd.
 	SYSTEMMODAL, 0x00001000
+	/// Same as [`MB::APPLMODAL`](crate::co::MB::APPLMODAL) except that all the
+	/// top-level windows belonging to the current thread are disabled if the
+	/// hWnd parameter is NULL. Use this flag when the calling application or
+	/// library does not have a window handle available but still needs to
+	/// prevent input to other windows in the calling thread without suspending
+	/// other threads.
 	TASKMODAL, 0x00002000
 
+	/// Same as desktop of the interactive window station. For more information,
+	/// see
+	/// [Window Stations](https://docs.microsoft.com/en-us/windows/win32/winstation/window-stations).
+	///
+	/// If the current input desktop is not the default desktop,
+	/// [`MessageBox`](crate::HWND::MessageBox) does not return until the user
+	/// switches to the default desktop.
 	DEFAULT_DESKTOP_ONLY, 0x00020000
+	/// The text is right-justified.
 	RIGHT, 0x00080000
+	/// Displays message and caption text using right-to-left reading order on
+	/// Hebrew and Arabic systems.
 	RTLREADING, 0x00100000
+	/// The message box becomes the foreground window. Internally, the system
+	/// calls the [`SetForegroundWindow`](crate::HWND::SetForegroundWindow)
+	/// function for the message box.
 	SETFOREGROUND, 0x00010000
+	/// The message box is created with the
+	/// [`WS_EX::TOPMOST`](crate::co::WS_EX::TOPMOST) window style.
 	TOPMOST, 0x00040000
+	/// The caller is a service notifying the user of an event. The function
+	/// displays a message box on the current active desktop, even if there is
+	/// no user logged on to the computer.
+	///
+	/// Terminal Services: If the calling thread has an impersonation token, the
+	/// function directs the message box to the session specified in the
+	/// impersonation token.
+	///
+	/// If this flag is set, the `hWnd` parameter must be NULL. This is so that
+	/// the message box can appear on a desktop other than the desktop
+	/// corresponding to the `hWnd`.
+	///
+	/// For information on security considerations in regard to using this flag,
+	/// see
+	/// [Interactive Services](https://docs.microsoft.com/en-us/windows/win32/services/interactive-services).
+	/// In particular, be aware that this flag can produce interactive content
+	/// on a locked desktop and should therefore be used for only a very limited
+	/// set of scenarios, such as resource exhaustion.
 	SERVICE_NOTIFICATION, 0x00200000
 }
 
