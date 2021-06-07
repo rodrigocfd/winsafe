88edae7dc0b43c7dc18e4ed4339864f695d9357a
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-06-07 15:08:33 -0300

    AM_MEDIA_TYPE DiretShow struct.

--- src/com/dshow/any_structs.rs
+++ src/com/dshow/any_structs.rs
@@ -1,5 +1,67 @@
 #![allow(non_snake_case)]
 
+use std::marker::PhantomData;
+
+use crate::ffi::BOOL;
+use crate::structs::GUID;
+
+/// [`AM_MEDIA_TYPE`](https://docs.microsoft.com/en-us/windows/win32/api/strmif/ns-strmif-am_media_type)
+/// struct.
+#[repr(C)]
+pub struct AM_MEDIA_TYPE<'a> {
+	pub majortype: GUID,
+	pub subtype: GUID,
+	bFixedSizeSamples: BOOL,
+	bTemporalCompression: BOOL,
+	pub lSampleSize: u32,
+	pub formattype: GUID,
+	pUnk: usize,
+	cbFormat: u32,
+	pbFormat: usize,
+	m_pbFormat: PhantomData<&'a usize>,
+}
+
+impl_default_zero!(AM_MEDIA_TYPE, 'a);
+
+impl<'a> AM_MEDIA_TYPE<'a> {
+	/// Returns the `bFixedSizeSamples` field.
+	pub fn bFixedSizeSamples(&self) -> bool {
+		self.bFixedSizeSamples != 0
+	}
+
+	/// Sets the `bFixedSizeSamples` field.
+	pub fn set_bFixedSizeSamples(&mut self, val: bool) {
+		self.bFixedSizeSamples = val as _;
+	}
+
+	/// Returns the `bTemporalCompression` field.
+	pub fn bTemporalCompression(&self) -> bool {
+		self.bTemporalCompression != 0
+	}
+
+	/// Sets the `bTemporalCompression` field.
+	pub fn set_bTemporalCompression(&mut self, val: bool) {
+		self.bTemporalCompression = val as _;
+	}
+
+	/// Returns the `pbFormat` field.
+	///
+	/// Varies according to the `formattype`. If you set it wrong, you're likely
+	/// to cause a buffer overrun.
+	pub unsafe fn pbFormat<T>(&self) -> &T {
+		&*(self.pbFormat as *const T)
+	}
+
+	/// Sets the `pbFormat` field.
+	///
+	/// Varies according to the `formattype`. If you set it wrong, you're likely
+	/// to cause a buffer overrun.
+	pub unsafe fn set_pbFormat<T>(&mut self, val: &'a mut T) {
+		self.pbFormat = val as *mut _ as _;
+		self.cbFormat = std::mem::size_of::<T>() as _;
+	}
+}
+
 /// [`MFVideoNormalizedRect`](https://docs.microsoft.com/en-us/windows/win32/api/evr/ns-evr-mfvideonormalizedrect)
 /// struct.
 #[repr(C)]
