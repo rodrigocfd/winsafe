fcf051d31b4fb60ddca0741219999eb677c8a0d3
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-11-16 10:44:44 -0300

    ResourceInfoBlock abstraction.

--- src/various/resource_info.rs
+++ src/various/resource_info.rs
@@ -15,10 +15,10 @@ use crate::various::WString;
 /// use winsafe::{HINSTANCE, ResourceInfo};
 ///
 /// let exe_name = HINSTANCE::NULL.GetModuleFileName()?;
-/// let ri = ResourceInfo::read_from(&exe_name)?;
+/// let res_info = ResourceInfo::read_from(&exe_name)?;
 ///
-/// if let Some(ffi) = ri.fixed_file_info() {
-///     let ver = ffi.dwFileVersion();
+/// if let Some(ver_info) = res_info.version_info() {
+///     let ver = ver_info.dwFileVersion();
 ///     println!("Version: {}.{}.{}.{}",
 ///         ver[0], ver[1], ver[2], ver[3]);
 /// }
@@ -32,33 +32,19 @@ use crate::various::WString;
 /// use winsafe::{HINSTANCE, ResourceInfo};
 ///
 /// let exe_name = HINSTANCE::NULL.GetModuleFileName()?;
-/// let ri = ResourceInfo::read_from(&exe_name)?;
+/// let res_info = ResourceInfo::read_from(&exe_name)?;
 ///
-/// if let Some(langs_cps) = ri.langs_and_code_pages() {
-///     for (lang, code_page) in langs_cps.iter() {
-///
-///         if let Some(product_name) = ri.product_name(*lang, *code_page) {
-///             println!("Product name: {}", product_name);
-///         }
-///
-///         if let Some(copyright) = ri.legal_copyright(*lang, *code_page) {
-///             println!("Copyright: {}", copyright);
-///         }
-///
+/// for block in res_info.blocks() {
+///     if let Some(product_name) = block.product_name() {
+///         println!("Product name: {}", product_name);
+///     }
+///     if let Some(copyright) = block.copyright() {
+///         println!("Copyright: {}", copyright);
 ///     }
 /// }
 /// ```
 pub struct ResourceInfo {
 	res_buf: Vec<u8>,
-}
-
-macro_rules! pub_fn_string_info {
-	($fun:ident, $name:expr) => {
-		/// Retrieves the version information string, if any.
-		pub fn $fun(&self, lang: LANGID, code_page: co::CP) -> Option<String> {
-			self.generic_string_info(lang, code_page, $name)
-		}
-	};
 }
 
 impl ResourceInfo {
@@ -67,50 +53,74 @@ impl ResourceInfo {
 		Ok(Self { res_buf: GetFileVersionInfo(exe_file)? })
 	}
 
+	/// Returns an iterator over the language blocks, if any, which are composed
+	/// of a language ID and a code page.
+	///
+	/// These blocks allow retrieval of version information strings in their
+	/// respective languages.
+	pub fn blocks(&self) -> impl Iterator<Item = ResourceInfoBlock> {
+		unsafe {
+			VarQueryValue::<(LANGID, co::CP)>(&self.res_buf, "\\VarFileInfo\\Translation")
+				.ok()
+				.map(|(plangs, sz)|
+					std::slice::from_raw_parts(
+						plangs,
+						sz as usize / std::mem::size_of::<(LANGID, co::CP)>(),
+					)
+				)
+				.unwrap_or(&[])
+				.iter()
+				.map(|(lang_id, code_page)|
+					ResourceInfoBlock {
+						res_info: self,
+						lang_id: *lang_id,
+						code_page: *code_page,
+					}
+				)
+		}
+	}
+
 	/// Returns the version information, if any.
-	pub fn fixed_file_info(&self) -> Option<&VS_FIXEDFILEINFO> {
+	pub fn version_info(&self) -> Option<&VS_FIXEDFILEINFO> {
 		unsafe {
 			VarQueryValue::<VS_FIXEDFILEINFO>(&self.res_buf, "\\")
 				.ok()
 				.map(|(pvsf, _)| &*pvsf)
 		}
 	}
+}
 
-	/// Return the languages and code pages available in the resource.
-	pub fn langs_and_code_pages(&self) -> Option<&[(LANGID, co::CP)]> {
-		unsafe {
-			VarQueryValue::<(LANGID, co::CP)>(&self.res_buf, "\\VarFileInfo\\Translation")
-				.ok()
-				.map(|(plangs, sz)| {
-					std::slice::from_raw_parts(
-						plangs,
-						sz as usize / std::mem::size_of::<(LANGID, co::CP)>(),
-					)
-				})
-		}
-	}
+/// An language block of [`ResourceInfo`](crate::ResourceInfo), composed of a
+/// language ID and a code page.
+pub struct ResourceInfoBlock<'a> {
+	res_info: &'a ResourceInfo,
+	lang_id: LANGID,
+	code_page: co::CP,
+}
 
-	pub_fn_string_info!(comments, "Comments");
-	pub_fn_string_info!(company_name, "CompanyName");
-	pub_fn_string_info!(file_description, "FileDescription");
-	pub_fn_string_info!(file_version, "FileVersion");
-	pub_fn_string_info!(internal_name, "InternalName");
-	pub_fn_string_info!(legal_copyright, "LegalCopyright");
-	pub_fn_string_info!(legal_trademarks, "LegalTrademarks");
-	pub_fn_string_info!(original_filename, "OriginalFilename");
-	pub_fn_string_info!(product_name, "ProductName");
-	pub_fn_string_info!(product_version, "ProductVersion");
-	pub_fn_string_info!(private_build, "PrivateBuild");
-	pub_fn_string_info!(special_build, "SpecialBuild");
+impl<'a> ResourceInfoBlock<'a> {
+	pub const fn lang_id(&self) -> LANGID { self.lang_id }
+	pub const fn code_page(&self) -> co::CP { self.code_page }
 
-	fn generic_string_info(&self,
-		lang: LANGID, code_page: co::CP, info: &str) -> Option<String>
-	{
+	pub fn comments(&self) -> Option<String> { self.generic_string_info("Comments") }
+	pub fn company_name(&self) -> Option<String> { self.generic_string_info("CompanyName") }
+	pub fn file_description(&self) -> Option<String> { self.generic_string_info("FileDescrition") }
+	pub fn file_version(&self) -> Option<String> { self.generic_string_info("FileVersion") }
+	pub fn internal_name(&self) -> Option<String> { self.generic_string_info("InternalName") }
+	pub fn legal_copyright(&self) -> Option<String> { self.generic_string_info("LegalCopyright") }
+	pub fn legal_trademarks(&self) -> Option<String> { self.generic_string_info("LegalTrademarks") }
+	pub fn original_filename(&self) -> Option<String> { self.generic_string_info("OriginalFilename") }
+	pub fn product_name(&self) -> Option<String> { self.generic_string_info("ProductName") }
+	pub fn product_version(&self) -> Option<String> { self.generic_string_info("ProductVersion") }
+	pub fn private_build(&self) -> Option<String> { self.generic_string_info("PrivateBuild") }
+	pub fn special_build(&self) -> Option<String> { self.generic_string_info("SpecialBuild") }
+
+	fn generic_string_info(&self, info: &str) -> Option<String> {
 		unsafe {
 			VarQueryValue::<u16>(
-				&self.res_buf,
+				&self.res_info.res_buf,
 				&format!("\\StringFileInfo\\{:04x}{:04x}\\{}",
-					u16::from(lang), u16::from(code_page), info),
+					u16::from(self.lang_id), u16::from(self.code_page), info),
 			).ok()
 				.map(|(pstr, len)| {
 					WString::from_wchars_slice(
