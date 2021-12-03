f1eafdf6f41e97bcc3ca1925ae90e03d3b9bdcc2
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2021-12-02 22:14:12 -0300

    IStream COM interface.

--- src/com/idl/mod.rs
+++ src/com/idl/mod.rs
@@ -13,15 +13,18 @@ pub mod co;
 pub(in crate::com) mod ipersist;
 pub(in crate::com) mod ipicture;
 pub(in crate::com) mod isequentialstream;
+pub(in crate::com) mod istream;
 
 pub use ipersist::IPersist;
 pub use ipicture::IPicture;
 pub use isequentialstream::ISequentialStream;
+pub use istream::IStream;
 
 pub(crate) mod prelude {
 	pub use super::ipersist::IPersistT;
 	pub use super::ipicture::IPictureT;
 	pub use super::isequentialstream::ISequentialStreamT;
+	pub use super::istream::IStreamT;
 }
 
 /// [IDL](https://docs.microsoft.com/en-us/windows/win32/api/_com/) COM
@@ -30,4 +33,5 @@ pub mod vt {
 	pub use super::ipersist::IPersistVT;
 	pub use super::ipicture::IPictureVT;
 	pub use super::isequentialstream::ISequentialStreamVT;
+	pub use super::istream::IStreamVT;
 }
