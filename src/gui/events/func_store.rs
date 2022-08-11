f43df6017e813dc1969d05b06b93b5d40d1ca615
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2022-08-11 00:55:13 -0300

    Clearer FuncStore innards.

--- src/gui/events/func_store.rs
+++ src/gui/events/func_store.rs
@@ -1,13 +1,19 @@
 use crate::gui::very_unsafe_cell::VeryUnsafeCell;
 
+/// An identifier and a closure.
+struct Pair<K: Copy + Eq, F> {
+	id: K,
+	func: F,
+}
+
 /// Stores closures, associating them with an identifier.
 pub(in crate::gui) struct FuncStore<K: Copy + Eq, F> {
-	elems: VeryUnsafeCell<Vec<(K, F)>>,
+	elems: VeryUnsafeCell<Vec<Pair<K, F>>>,
 }
 
 impl<K: Copy + Eq, F> FuncStore<K, F> {
 	/// Creates a new, empty store.
-	pub(in crate::gui) fn new() -> FuncStore<K, F> {
+	pub(in crate::gui) fn new() -> Self {
 		Self { elems: VeryUnsafeCell::new(Vec::default()) }
 	}
 
@@ -17,7 +23,7 @@ impl<K: Copy + Eq, F> FuncStore<K, F> {
 		if elems.is_empty() {
 			elems.reserve(16); // arbitrary, prealloc for speed
 		}
-		elems.push((id, func));
+		elems.push(Pair { id, func });
 	}
 
 	/// Finds the last added function associated to the given identifier, if
@@ -26,8 +32,8 @@ impl<K: Copy + Eq, F> FuncStore<K, F> {
 		// Linear search, more performant for small collections.
 		// Searches backwards, so the function added last will be chosen.
 		self.elems.iter().rev()
-			.find(move |elem| elem.0 == id)
-			.map(|elem| &elem.1)
+			.find(move |elem| elem.id == id)
+			.map(|elem| &elem.func)
 	}
 
 	/// Finds all the functions associated to the given identifier, if any, and
@@ -35,8 +41,8 @@ impl<K: Copy + Eq, F> FuncStore<K, F> {
 	pub(in crate::gui) fn find_all(&self, id: K) -> impl Iterator<Item = &F> {
 		// https://depth-first.com/articles/2020/06/22/returning-rust-iterators
 		self.elems.iter()
-			.filter(move |elem| elem.0 == id)
-			.map(|elem| &elem.1)
+			.filter(move |elem| elem.id == id)
+			.map(|elem| &elem.func)
 	}
 
 	/// Tells whether no functions have been added.
