8b2d9f7ad7585be12a5bdafa48a08d6a7de13997
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-06-16 21:09:55 -0300

    IDXGI method.

--- src/dxgi/com_interfaces/idxgiobject.rs
+++ src/dxgi/com_interfaces/idxgiobject.rs
@@ -1,5 +1,6 @@
 #![allow(non_camel_case_types, non_snake_case)]
 
+use crate::kernel::decl::GUID;
 use crate::kernel::ffi_types::{COMPTR, HRES, PCVOID, PVOID};
 use crate::ole::decl::HrResult;
 use crate::ole::privs::{ok_to_hrresult, vt};
@@ -10,7 +11,7 @@ use crate::vt::IUnknownVT;
 #[repr(C)]
 pub struct IDXGIObjectVT {
 	pub IUnknownVT: IUnknownVT,
-	pub SetPrivateData: fn(COMPTR, PCVOID, u32, PVOID) -> HRES,
+	pub SetPrivateData: fn(COMPTR, PCVOID, u32, PCVOID) -> HRES,
 	pub SetPrivateDataInterface: fn(COMPTR, PCVOID, COMPTR) -> HRES,
 	pub GetPrivateData: fn(COMPTR, PCVOID, *mut u32, PVOID) -> HRES,
 	pub GetParent: fn(COMPTR, PCVOID, *mut COMPTR) -> HRES,
@@ -45,7 +46,7 @@ pub trait dxgi_IDXGIObject: ole_IUnknown {
 		let mut queried = unsafe { T::null() };
 		ok_to_hrresult(
 			unsafe {
-				(vt::<IUnknownVT>(self).QueryInterface)(
+				(vt::<IDXGIObjectVT>(self).GetParent)(
 					self.ptr(),
 					&T::IID as *const _ as _,
 					queried.as_mut(),
@@ -54,9 +55,28 @@ pub trait dxgi_IDXGIObject: ole_IUnknown {
 		).map(|_| queried)
 	}
 
+	/// [`IDXGIObject::SetPrivateData`](https://learn.microsoft.com/en-us/windows/win32/api/dxgi/nf-dxgi-idxgiobject-setprivatedata)
+	/// method.
+	///
+	/// Note: a copy of the data is made.
+	fn SetPrivateData<T>(&self, name: &GUID, data: &T) -> HrResult<()>
+		where T: Sized,
+	{
+		ok_to_hrresult(
+			unsafe {
+				(vt::<IDXGIObjectVT>(self).SetPrivateData)(
+					self.ptr(),
+					name as *const _ as _,
+					std::mem::size_of::<T>() as _,
+					data as *const _ as _,
+				)
+			},
+		)
+	}
+
 	/// [`IDXGIObject::SetPrivateDataInterface`](https://learn.microsoft.com/en-us/windows/win32/api/dxgi/nf-dxgi-idxgiobject-setprivatedatainterface)
 	/// method.
-	fn SetPrivateDataInterface<T>(&self, interface: &T) -> HrResult<()>
+	fn SetPrivateDataInterface<T>(&self, obj: &T) -> HrResult<()>
 		where T: ole_IUnknown,
 	{
 		ok_to_hrresult(
@@ -64,7 +84,7 @@ pub trait dxgi_IDXGIObject: ole_IUnknown {
 				(vt::<IDXGIObjectVT>(self).SetPrivateDataInterface)(
 					self.ptr(),
 					&T::IID as *const _ as _,
-					interface.ptr(),
+					obj.ptr(),
 				)
 			},
 		)
