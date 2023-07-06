c7744b1d35079cf77dd8ef972eecf1d8f824f025
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-07-06 10:23:59 -0300

    Using verbatim linking modifier for .res file.

--- build.rs
+++ build.rs
@@ -1,19 +1,3 @@
-use std::fs;
-use std::path::Path;
-
 fn main() {
-	const RES: &str = "resources/resources.res";
-
-	// Create a copy of .res file, and append .lib extension.
-	// This is necessary because of a limitation in the toolchain.
-	// https://github.com/rust-lang/rust/issues/81488
-
-	let mut dotlib = String::from(RES);
-	dotlib.push_str(".lib");
-
-	if Path::new(RES).exists() {
-		fs::copy(RES, &dotlib).unwrap();
-	}
-
-	println!("cargo:rustc-link-lib=dylib={}", RES);
+	println!("cargo:rustc-link-lib=dylib:+verbatim=resources/resources.res");
 }
