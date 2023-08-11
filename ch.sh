df3fb2fbb23ea67f080cd4c8dc7e0e3bd77738a7
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-08-11 09:15:25 -0300

    Shell script timings.

--- ch.sh
+++ ch.sh
@@ -20,7 +20,26 @@ FEATS=(
 
 set -e
 
+print_elapsed () {
+	MIN=$(( ($1 - ($1 % (60 * 1000))) / (1000 * 60) ))
+	SEC=$(( ($TF - ($MIN * 1000 * 60) - ($1 % 1000)) / 1000 ))
+	MS=$(( $1 % 1000 ))
+
+	PURP='\033[0;35m'
+	NC='\033[0m'
+	if (($MIN > 0)); then
+		printf "    ${PURP}Duration${NC} %02d:%02d.%03d min\n" $MIN $SEC $MS
+	else
+		printf "    ${PURP}Duration${NC} %d.%03d sec\n" $SEC $MS
+	fi
+}
+
 for FEAT in "${FEATS[@]}" ; do
 	echo "$FEAT..."
+	T0=$(date +%s%N)
+
 	cargo check --features "$FEAT"
+
+	TF=$((($(date +%s%N) - $T0)/1000000))
+	print_elapsed $TF
 done
--- gh.sh
+++ gh.sh
@@ -2,6 +2,8 @@
 # which must be in the $GHP directory.
 # https://gist.github.com/rodrigocfd/3a0f3370817ec5c8c3d2ec6e516ae86b
 
+T0=$(date +%s%N)
+
 GHP=../gh-pages-winsafe # target gh-pages repo folder
 
 echo '> Generating docs locally...'
@@ -29,3 +31,20 @@ git push
 echo '> Updating local repo...'
 cd -
 git pull
+
+print_elapsed () {
+	MIN=$(( ($1 - ($1 % (60 * 1000))) / (1000 * 60) ))
+	SEC=$(( ($TF - ($MIN * 1000 * 60) - ($1 % 1000)) / 1000 ))
+	MS=$(( $1 % 1000 ))
+
+	PURP='\033[0;35m'
+	NC='\033[0m'
+	if (($MIN > 0)); then
+		printf "${PURP}Duration${NC} %02d:%02d.%03d min\n" $MIN $SEC $MS
+	else
+		printf "${PURP}Duration${NC} %d.%03d sec\n" $SEC $MS
+	fi
+}
+
+TF=$((($(date +%s%N) - $T0)/1000000))
+print_elapsed $TF
