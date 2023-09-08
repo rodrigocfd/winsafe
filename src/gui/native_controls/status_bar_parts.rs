b12414e77c1e65be329b5be5c62e9f726d36f333
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-09-08 09:35:03 -0300

    Set text to multiple status bar parts.

--- src/gui/native_controls/status_bar_parts.rs
+++ src/gui/native_controls/status_bar_parts.rs
@@ -32,4 +32,42 @@ impl<'a> StatusBarParts<'a> {
 	pub const fn get(&self, index: u8) -> StatusBarPart<'a> {
 		StatusBarPart::new(self.owner, index)
 	}
+
+	/// Sets the texts of multiple parts at once.
+	///
+	/// # Panics
+	///
+	/// Panics if `texts` is empty, or if the number of texts is greater than
+	/// the number of columns.
+	///
+	/// # Examples
+	///
+	/// ```no_run
+	/// use winsafe::{self as w, prelude::*, gui};
+	///
+	/// let my_sb: gui::StatusBar; // initialized somewhere
+	/// # let wnd = gui::WindowMain::new(gui::WindowMainOpts::default());
+	/// # let my_sb = gui::StatusBar::new(&wnd, &[]);
+	///
+	/// my_sb.parts().set_texts(&[
+	///     Some("First"),
+	///     None, // 2nd part won't have its text changed
+	///     Some("Third"),
+	///     Some("Fourth"),
+	/// ]);
+	/// ```
+	pub fn set_texts(&self, texts: &[Option<impl AsRef<str>>]) {
+		if texts.is_empty() {
+			panic!("No texts passed when setting StatusBar parts text.");
+		} else if texts.len() > self.count() as usize {
+			panic!("Cannot set {} text(s) to {} part(s).", texts.len(), self.count());
+		}
+
+		texts.iter()
+			.enumerate()
+			.for_each(|(idx, maybe_text)| {
+				maybe_text.as_ref()
+					.map(|text| self.get(idx as _).set_text(text.as_ref()));
+			});
+	}
 }
