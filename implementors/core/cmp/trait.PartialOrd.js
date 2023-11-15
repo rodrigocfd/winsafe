1c77d2e8b3d70e8d3a397d9fd6071b7a81bafdb7
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2023-11-15 11:38:43 -0300

    Cargo doc auto deployment 2023-11-15 11:38:39.

--- implementors/core/cmp/trait.PartialOrd.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {
-"winsafe":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"winsafe/struct.WString.html\" title=\"struct winsafe::WString\">WString</a>&gt; for <a class=\"struct\" href=\"winsafe/struct.WString.html\" title=\"struct winsafe::WString\">WString</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"winsafe":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"winsafe/struct.WString.html\" title=\"struct winsafe::WString\">WString</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
