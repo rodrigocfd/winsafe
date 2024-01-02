423c89664db29d4e84f77f56d4bb86c0340ec288
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-01-02 08:05:12 -0300

    Cargo doc auto deployment 2024-01-02 08:05:09.

---
+++ trait.impl/winsafe/gui/events/window_events_all/trait.GuiEventsAll.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"winsafe":[]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
