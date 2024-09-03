da1486af5f223eefa67eec5a583a28ed2839810e
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2024-09-03 15:39:21 -0300

    Cargo doc auto deployment 2024-09-03 15:39:11.

--- trait.impl/core/convert/trait.TryFrom.js
+++ trait.impl/core/convert/trait.TryFrom.js
@@ -1,3 +1,9 @@
-(function() {var implementors = {
-"winsafe":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"winsafe/co/struct.SE_PRIV.html\" title=\"struct winsafe::co::SE_PRIV\">SE_PRIV</a>"]]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {
+    var implementors = Object.fromEntries([["winsafe",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.TryFrom.html\" title=\"trait core::convert::TryFrom\">TryFrom</a>&lt;&amp;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"winsafe/co/struct.SE_PRIV.html\" title=\"struct winsafe::co::SE_PRIV\">SE_PRIV</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[390]}
