1d8606bea4ab15e52f2e3ae1c784252de5263e94
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2025-06-22 12:32:37 -0300

    No event bind macros.

--- src/dlg_main.rs
+++ src/dlg_main.rs
@@ -1,5 +1,5 @@
 use std::rc::Rc;
-use winsafe::{self as w, bind, bind_ig, co, gui, msg, prelude::*};
+use winsafe::{self as w, co, gui, msg, prelude::*};
 
 use crate::{ids, stats};
 
@@ -42,80 +42,80 @@ impl DlgMain {
 	}
 
 	fn events(&self) {
+		let self2 = self.clone();
+		self.wnd.on().wm_init_dialog(move |_| {
+			let target_dir = "D:\\Stuff\\Core\\rs\\winsafe\\src"; // arbitrary initial dir
+			self2.txt_path.set_text(target_dir)?;
+
+			unsafe {
+				self2.txt_out.hwnd().SendMessage(msg::wm::SetFont {
+					hfont: self2.mono_font.raw_copy(), // fixed-width font for output
+					redraw: true,
+				});
+			}
+
+			Ok(true)
+		});
+
+		let self2 = self.clone();
 		self.wnd
 			.on()
-			.wm_init_dialog(bind_ig!(self, Self::on_init_dialog))
-			.wm_command_acc_menu(co::DLGID::CANCEL, bind!(self, Self::on_esc));
-		self.btn_run.on().bn_clicked(bind!(self, Self::on_run));
+			.wm_command_acc_menu(co::DLGID::CANCEL, move || {
+				self2.wnd.close(); // close window on Esc
+				Ok(())
+			});
+
+		let self2 = self.clone();
+		self.btn_run.on().bn_clicked(move || {
+			self2.txt_path.hwnd().EnableWindow(false);
+			self2.btn_run.hwnd().EnableWindow(false);
+			self2.txt_out.set_text("")?;
+
+			let target_dir = self2.txt_path.text()?;
+			if !w::path::exists(&target_dir) {
+				w::TaskDialogIndirect(&w::TASKDIALOGCONFIG {
+					hwnd_parent: Some(self2.wnd.hwnd()),
+					window_title: Some("Bad path"),
+					main_instruction: Some("Process cannot be done"),
+					content: Some(&format!("Path does not exist:\n{}", target_dir)),
+					main_icon: w::IconIdTd::Td(co::TD_ICON::ERROR),
+					common_buttons: co::TDCBF::OK,
+					flags: co::TDF::ALLOW_DIALOG_CANCELLATION
+						| co::TDF::POSITION_RELATIVE_TO_WINDOW,
+					..Default::default()
+				})?;
+				return Ok(()); // halt processing
+			}
+
+			self2.pro_load.set_marquee(true);
+			let total_files_count = w::path::dir_walk(&target_dir).count(); // how many files to process?
+			self2.pro_load.set_marquee(false);
+
+			self2.pro_load.set_range(0, total_files_count as _); // setup progress bar
+			self2.pro_load.set_position(0);
+
+			let titlebar_text = self2.wnd.hwnd().GetWindowText()?;
+			self2.wnd.hwnd().SetWindowText(&format!(
+				"{} - {} files",
+				titlebar_text.split('-').next().unwrap().trim_end(),
+				self2.pro_load.range().1,
+			))?;
+
+			let self3 = self2.clone();
+			let stats = stats::gather(&target_dir, move |pass_idx| {
+				self3.pro_load.set_position(pass_idx as _); // process the files
+			})?;
+
+			self2.txt_out.set_text(&stats.to_string())?;
+			self2.txt_out.focus()?;
+
+			self2.txt_path.hwnd().EnableWindow(true);
+			self2.btn_run.hwnd().EnableWindow(true);
+			Ok(())
+		});
 	}
 
 	pub fn run(&self) -> w::AnyResult<i32> {
 		self.wnd.run_main(None)
-	}
-
-	fn on_init_dialog(&self) -> w::AnyResult<bool> {
-		let target_dir = "D:\\Stuff\\Core\\rs\\winsafe\\src"; // arbitrary initial dir
-		self.txt_path.set_text(target_dir)?;
-
-		unsafe {
-			self.txt_out.hwnd().SendMessage(msg::wm::SetFont {
-				hfont: self.mono_font.raw_copy(), // fixed-width font for output
-				redraw: true,
-			});
-		}
-
-		Ok(true)
-	}
-
-	fn on_esc(&self) -> w::AnyResult<()> {
-		self.wnd.close(); // close window on Esc
-		Ok(())
-	}
-
-	fn on_run(&self) -> w::AnyResult<()> {
-		self.txt_path.hwnd().EnableWindow(false);
-		self.btn_run.hwnd().EnableWindow(false);
-		self.txt_out.set_text("")?;
-
-		let target_dir = self.txt_path.text()?;
-		if !w::path::exists(&target_dir) {
-			w::TaskDialogIndirect(&w::TASKDIALOGCONFIG {
-				hwnd_parent: Some(self.wnd.hwnd()),
-				window_title: Some("Bad path"),
-				main_instruction: Some("Process cannot be done"),
-				content: Some(&format!("Path does not exist:\n{}", target_dir)),
-				main_icon: w::IconIdTd::Td(co::TD_ICON::ERROR),
-				common_buttons: co::TDCBF::OK,
-				flags: co::TDF::ALLOW_DIALOG_CANCELLATION | co::TDF::POSITION_RELATIVE_TO_WINDOW,
-				..Default::default()
-			})?;
-			return Ok(()); // halt processing
-		}
-
-		self.pro_load.set_marquee(true);
-		let total_files_count = w::path::dir_walk(&target_dir).count(); // how many files to process?
-		self.pro_load.set_marquee(false);
-
-		self.pro_load.set_range(0, total_files_count as _); // setup progress bar
-		self.pro_load.set_position(0);
-
-		let titlebar_text = self.wnd.hwnd().GetWindowText()?;
-		self.wnd.hwnd().SetWindowText(&format!(
-			"{} - {} files",
-			titlebar_text.split('-').next().unwrap().trim_end(),
-			self.pro_load.range().1,
-		))?;
-
-		let self2 = self.clone();
-		let stats = stats::gather(&target_dir, move |pass_idx| {
-			self2.pro_load.set_position(pass_idx as _); // process the files
-		})?;
-
-		self.txt_out.set_text(&stats.to_string())?;
-		self.txt_out.focus()?;
-
-		self.txt_path.hwnd().EnableWindow(true);
-		self.btn_run.hwnd().EnableWindow(true);
-		Ok(())
 	}
 }
