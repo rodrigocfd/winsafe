496defb067468a9b7bfed00ff25bb4bcaf20af14
Rodrigo Cesar de Freitas Dias <rcesar@gmail.com>
2025-07-15 08:42:49 -0300

    ReadFile/WriteFile for HSTD.

--- src/kernel/handles/hstd.rs
+++ src/kernel/handles/hstd.rs
@@ -76,6 +76,19 @@ impl HSTD {
 		.map(|_| num_read)
 	}
 
+	/// [`ReadFile`](https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-readfile)
+	/// function.
+	///
+	/// Returns the number of bytes read.
+	///
+	/// Note that asynchronous reading – which use the
+	/// [`OVERLAPPED`](crate::OVERLAPPED) struct – is not currently supported by
+	/// this method, because the buffer must remain untouched until the async
+	/// operation is complete, thus making the method unsound.
+	pub fn ReadFile(&self, buffer: &mut [u8]) -> SysResult<u32> {
+		unsafe { HFILE::from_ptr(self.ptr()) }.ReadFile(buffer)
+	}
+
 	/// [`SetConsoleMode`](https://learn.microsoft.com/en-us/windows/console/setconsolemode)
 	/// function.
 	pub fn SetConsoleMode(&self, mode: co::CONSOLE) -> SysResult<()> {
@@ -100,5 +113,18 @@ impl HSTD {
 			))
 		}
 		.map(|_| num_written)
+	}
+
+	/// [`WriteFile`](https://learn.microsoft.com/en-us/windows/win32/api/fileapi/nf-fileapi-writefile)
+	/// function.
+	///
+	/// Returns the number of bytes written.
+	///
+	/// Note that asynchronous writing – which use the
+	/// [`OVERLAPPED`](crate::OVERLAPPED) struct – is not currently supported by
+	/// this method, because the buffer must remain untouched until the async
+	/// operation is complete, thus making the method unsound.
+	pub fn WriteFile(&self, data: &[u8]) -> SysResult<u32> {
+		unsafe { HFILE::from_ptr(self.ptr()) }.WriteFile(data)
 	}
 }
